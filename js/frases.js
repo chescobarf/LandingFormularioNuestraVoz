58298f283cae5c0ff521091d8af80b7a71c65dc1
Christian Escobar <christian17zxe@gmail.com>
2020-06-23 00:10:14 -0400

    MouserOver para Cambios de Frases

---
+++ js/frases.js
@@ -0,0 +1,28 @@
+
+let frases = [
+    `"Un Chile mas inclusivo"`,
+    `"Un pais con las mismas posibilidades para todas"`,
+    `"Un Chile seguro para todas"`,
+    `"Con politicas mas justas"`,
+    `"Que se respeten todas las minorias sexuales"`,
+    `"Salir sin miedos"`,
+    `"Ser tratada igual que todos"`
+]
+
+let frase=document.querySelector("#frase");
+let container=document.querySelector("#containerFrases")
+
+eventListener();
+
+function eventListener(){
+    frase.addEventListener('mouseover',cambiarFrases)
+}
+
+function cambiarFrases(){
+    aleat = Math.random() * (frases.length)
+    aleat = Math.floor(aleat)
+    frase.textContent=frases[aleat];
+  
+}
+
+
