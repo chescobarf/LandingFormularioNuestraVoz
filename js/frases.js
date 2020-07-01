c4ebf11f7fd48378728ed85f9095f9c20825d1a7
Christian Escobar <christian17zxe@gmail.com>
2020-07-01 10:56:49 -0400

    SEO y Minificado CSS

--- js/frases.js
+++ js/frases.js
@@ -11,25 +11,13 @@ let frases = [
 
 let frase=document.querySelector("#frase");
 let container=document.querySelector("#containerFrases")
-// eventListener();
 
-// function eventListener(){
-//     frase.addEventListener('mouseover',cambiarFrases);
-//     frase.addEventListener('click',cambiarFrases);
-// }
-
-// function cambiarFrases(){
-//     // aleat = Math.random() * (frases.length)
-//     // aleat = Math.floor(aleat)
-//     $("#frase").fadeToggle(1000,cambiar());
-//     // frase.textContent=frases[aleat]
-// }
 function cambiar(){
     aleat = Math.random() * (frases.length)
     aleat = Math.floor(aleat)
     frase.textContent=frases[aleat]
 }
-//setTimeout(cambiar, 2000);
+
 setInterval(cambiar, 1500);
 
 
